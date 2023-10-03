66f72b326e960ae33cc1588af8e30b7bde758a65
Olzhas <131268326+olzhassky@users.noreply.github.com>
2023-10-03 23:27:14 +0600

    Initial Commit

---
+++ gitTest/gitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitTestApp.swift
+//  gitTest
+//
+//  Created by Olzhas Zhakan on 03.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct gitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
